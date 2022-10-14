eab167d1c9ae614f8c0e665515a344eff0518091
KirylHritsiuk <pishime2017@gmail.com>
2022-10-14 12:54:20 +0300

    fix: tests FormPage, CardList

--- my-app/src/pages/FormPage/FormPage.test.tsx
+++ my-app/src/pages/FormPage/FormPage.test.tsx
@@ -14,36 +14,4 @@ describe('FormPage', () => {
     expect(screen.getByTestId('form')).toBeInTheDocument();
     expect(screen.queryByTestId('FormCard')).not.toBeInTheDocument();
   });
-
-  test('add card', () => {
-    render(
-      <MemoryRouter initialEntries={['/form']}>
-        <FormPage />
-      </MemoryRouter>
-    );
-    const name = screen.getByTestId('name');
-    const surname = screen.getByTestId('surname');
-    const zipCode = screen.getByTestId('zipCode');
-    const country = screen.getByTestId('country');
-    const birthday = screen.getByTestId('birthday');
-    const agree = screen.getByTestId('agree');
-    const button = screen.getByRole('button');
-    fireEvent.input(name, { target: { value: 'test' } });
-    fireEvent.input(surname, { target: { value: 'tester' } });
-    fireEvent.input(zipCode, { target: { value: '1234567' } });
-    fireEvent.change(country, { target: { value: 'Belarus' } });
-    fireEvent.input(birthday, { target: { value: '2000-02-02' } });
-    userEvent.click(agree);
-    // fireEvent.click(button);
-    // fireEvent.input(name, { target: { value: 'test' } });
-    // fireEvent.input(surname, { target: { value: 'test' } });
-    // fireEvent.input(zipCode, { target: { value: '1234567' } });
-    // fireEvent.change(country, { target: { value: 'Belarus' } });
-    // fireEvent.input(birthday, { target: { value: '2000-02-02' } });
-    // userEvent.click(agree);
-    // fireEvent.click(button);
-    expect(screen.getByText('tester')).toBe('tester');
-    // expect(screen.getByTestId('form')).toBeInTheDocument();
-    // expect(screen.getByTestId('FormCard')).toBeInTheDocument();
-  });
 });
