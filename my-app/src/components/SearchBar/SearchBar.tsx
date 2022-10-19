5dbb422e613863a238d267c3214343ad0376be5d
KirylHritsiuk <pishime2017@gmail.com>
2022-10-19 20:08:36 +0300

    feat: implement FC search bar

--- my-app/src/components/SearchBar/SearchBar.tsx
+++ my-app/src/components/SearchBar/SearchBar.tsx
@@ -1,67 +1,62 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { SearchBarProps } from './SearchBar.props';
 import { ReactComponent as SearchIcon } from './search.svg';
 import { Button, Filter } from 'components';
 import styles from './SearchBar.module.css';
 
-export class SearchBar extends React.Component<SearchBarProps> {
-  state = {
-    inputValue: localStorage.getItem('search') || '',
-    filter: '',
-  };
+export const SearchBar = ({
+  updateQuery,
+  updateFilter,
+  filter,
+  checked,
+}: SearchBarProps): JSX.Element => {
+  const localKey = 'search';
+  const [inputValue, setInputValue] = useState<string>(localStorage.getItem(localKey) || '');
 
-  componentDidMount(): void {
-    const inputValue = this.state.inputValue;
-    this.setState((prevState) => ({ ...prevState, inputValue }));
-    this.props.updateQuery(this.state.inputValue);
-  }
+  useEffect(() => {
+    return () => {
+      localStorage.setItem(localKey, inputValue);
+    };
+  }, [inputValue]);
 
-  componentWillUnmount(): void {
-    localStorage.setItem('search', this.state.inputValue);
-  }
-
-  onInputChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState((prevState) => ({ ...prevState, inputValue: e.target.value }));
-    localStorage.setItem('search', e.target.value);
-  }
-
-  search = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const search = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      this.props.updateQuery(this.state.inputValue);
+      updateQuery(inputValue);
       e.currentTarget.blur();
     }
   };
 
-  render(): React.ReactNode {
-    const { inputValue } = this.state;
-    const { updateQuery, updateFilter, filter, checked } = this.props;
-    return (
-      <div className={styles.searchBar}>
-        <div className={styles.search}>
-          <SearchIcon />
-          <input
-            data-testid="input"
-            placeholder="Search..."
-            className={styles.searchInput}
-            onChange={(e) => this.onInputChange(e)}
-            value={inputValue}
-            onKeyDown={(e) => this.search(e)}
-          />
-          <Button
-            appearance="primary"
-            className={styles.searchButton}
-            onClick={() => updateQuery(inputValue)}
-          >
-            Search
-          </Button>
-        </div>
-        <Filter
-          className={styles.filter}
-          names={filter}
-          checked={checked}
-          updateFilter={updateFilter}
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    localStorage.setItem(localKey, e.target.value);
+  };
+
+  return (
+    <div className={styles.searchBar}>
+      <div className={styles.search}>
+        <SearchIcon />
+        <input
+          data-testid="input"
+          placeholder="Search..."
+          className={styles.searchInput}
+          onChange={onInputChange}
+          value={inputValue}
+          onKeyDown={search}
         />
+        <Button
+          appearance="primary"
+          className={styles.searchButton}
+          onClick={() => updateQuery(inputValue)}
+        >
+          Search
+        </Button>
       </div>
-    );
-  }
-}
+      <Filter
+        className={styles.filter}
+        names={filter}
+        checked={checked}
+        updateFilter={updateFilter}
+      />
+    </div>
+  );
+};
