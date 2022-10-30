f8bd536097ab24732426b7871cfb9c89751b5fea
KirylHritsiuk <pishime2017@gmail.com>
2022-10-30 23:37:21 +0300

    feat: update pagination

--- my-app/src/components/UI/Pagination/Pagination.props.ts
+++ my-app/src/components/UI/Pagination/Pagination.props.ts
@@ -1,10 +1,4 @@
 import { HTMLAttributes, DetailedHTMLProps } from 'react';
 
 export interface PaginationProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  totalPages?: number;
-  page: number;
-  changePage: (url: string, page: number) => void;
-  prev: string | null;
-  next: string | null;
-}
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
--- my-app/src/components/UI/Pagination/Pagination.tsx
+++ my-app/src/components/UI/Pagination/Pagination.tsx
@@ -1,30 +1,66 @@
-import React from 'react';
-import { PaginationProps } from './Pagination.props';
 import styles from './Pagination.module.css';
-import cn from 'classnames';
-import { getPage } from './helpers/getPage';
-
-export class Pagination extends React.Component<PaginationProps> {
-  render() {
-    const { page, prev, next, changePage } = this.props;
-    return (
-      <div className={styles.pagination} data-testid="pagination">
-        {prev !== null ? (
-          <span className={styles.page} onClick={() => changePage(prev, page - 1)}>
-            prev
-          </span>
-        ) : (
-          <span className={cn(styles.page, styles.disabled)}>prev</span>
-        )}
-        <span className={styles.page}>{getPage(prev)}</span>
-        {next !== null ? (
-          <span className={styles.page} onClick={() => changePage(next, page + 1)}>
-            next
-          </span>
-        ) : (
-          <span className={cn(styles.page, styles.disabled)}>next</span>
-        )}
-      </div>
-    );
-  }
+import ReactPaginate from 'react-paginate';
+import { useFetching } from 'Hook/useFetching';
+import { PaginationProps } from './Pagination.props';
+interface PagOnClick {
+  index: number | null;
+  selected: number;
+  nextSelectedPage: number | undefined;
+  event: object;
+  isPrevious: boolean;
+  isNext: boolean;
+  isBreak: boolean;
+  isActive: boolean;
 }
+
+interface selectedItem {
+  selected: number;
+}
+export const Pagination = ({}: PaginationProps): JSX.Element => {
+  const { state, changePage } = useFetching();
+
+  const handlePageChange = ({ selected }: selectedItem) => {
+    changePage(
+      state.url +
+        state.category +
+        state.query +
+        state.pag +
+        `${selected + 1}` +
+        state.name +
+        state.search
+    );
+  };
+
+  const change = ({ isPrevious, isNext }: PagOnClick) => {
+    if (isNext && state.data.next) {
+      changePage(state.data.next);
+    } else if (isPrevious && state.data.prev) {
+      changePage(state.data.prev);
+    }
+  };
+
+  return (
+    <>
+      <ReactPaginate
+        previousLabel="Prev"
+        nextLabel="Next"
+        pageClassName={styles.page}
+        previousClassName={styles.page}
+        nextClassName={styles.page}
+        breakLabel="..."
+        breakClassName={styles.page}
+        pageCount={state.data.pages ? state.data.pages : 0}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        onClick={change}
+        containerClassName={styles.pagination}
+        activeClassName={styles.current}
+        disabledClassName={styles.disabled}
+        forcePage={state.data.page - 1}
+        prevPageRel={state.data.prev}
+        nextPageRel={state.data.next}
+      />
+    </>
+  );
+};
