1513443401fcb6524b9a6c8b3b42bace0c368460
KirylHritsiuk <pishime2017@gmail.com>
2022-11-02 15:34:51 +0300

    feat: start Redux task

--- my-app/src/Layout/Layout.tsx
+++ my-app/src/Layout/Layout.tsx
@@ -3,18 +3,16 @@ import Footer from 'Layout/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'Layout/Header/Header';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <>
-        <Header title="Task#5 React. Custom App State" />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </>
-    );
-  }
-}
+export const Layout = () => {
+  return (
+    <>
+      <Header title="Task#6 React.Redux" />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
